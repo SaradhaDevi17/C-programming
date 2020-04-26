c3d795cdb52da446c774d59f07bc5af2d93c454e
Saradhadevi <53566769+SaradhaDevi17@users.noreply.github.com>
2020-04-26 17:02:18 +0530

    Create long_keyword.c

---
+++ long_keyword.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    //demonstrate use of long keyword
+    int a;
+    printf("int size %d",sizeof(a));
+    long int b;
+     printf("\nlong int size %d",sizeof(b));
+    double c;
+    printf("\nDouble size %d",sizeof(c));
+    long double c1;
+    printf("\nLong double size %d",sizeof(c1));
+    return 0;
+    
+    
+}
