2ab1fba260eda57cde9d26f1b014681e303e76b5
Saradhadevi <53566769+SaradhaDevi17@users.noreply.github.com>
2020-04-26 15:48:11 +0530

    Create sizeof-datatypes.c

---
+++ sizeof-datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    //find size of various datatypes
+    int a;
+    float b;
+    double c;
+    char d;
+    printf("int=%d",sizeof(a));
+    printf("\nfloat=%d",sizeof(b));
+    printf("\ndouble=%d",sizeof(c));
+    printf("\nchar=%d",sizeof(d));
+    
+}
